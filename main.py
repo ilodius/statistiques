941023ac09c1fd5a02f2f9b5b74e9167f243d85d
ilodius <quentin.gueret2001@gmail.com>
2024-10-01 08:54:15 +0200

    fix

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-#import somme from sum_file
-#import moye from moy_file 
+from sum_file import affichesum
+from moyen_file.py import calcul_moyenne
 
 
-print (somme)
+nombres = [1.0, 2.0, 3.0]
+print("Data list :")
+print(nombres)
 
-print (moye) 
+print("---------------")
+somme, nombre_elements = affichesum(nombres)
+print("Somme :", somme)
+print("Nombre d'éléments :", nombre_elements)
+
+print("---------------")
+moyenne, min_val, max_val = calcul_moyenne(nombres)
+print("Moyenne :", moyenne)
+print("Min :", min_val)
+print("Max :", max_val)
